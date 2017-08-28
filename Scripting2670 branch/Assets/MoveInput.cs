fdeaaf92831e1fe8cbc8dd4b1261b27251848d11
Doctoremm <doctoremm@gmail.com>
2017-08-28 15:19:08 -0600

    trying out pen in photoshop

---
+++ Scripting2670 branch/Assets/MoveInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveInput : MonoBehaviour {
+
+    public static Action<float> KeyAction;
+
+	// Use this for initialization
+	void Update () {
+        if (KeyAction != null)
+        {
+            KeyAction(Input.GetAxis("Horizontal"));
+        }
+	}
+}
