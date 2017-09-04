7e388290992b7e3f409599a1c2c3292bded06668
Doctoremm <doctoremm@gmail.com>
2017-09-04 17:42:56 -0600

    Marty's game prototype.  I FIGURED OUT HOW TO CROUCH!!!!

---
+++ Game Prototype/Bear prototypes/Assets/Scripts/Death.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+    public Transform destination;
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            KillPlayer();
+        }
+    }
+    void KillPlayer()
+    {
+        player.transform.position = destination.position;
+    }
+}
---
+++ Game Prototype/Bear prototypes/Assets/Scripts/FlipCharacter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipCharacter : MonoBehaviour {
+
+    Quaternion myRotate;
+    Vector3 rotValue;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayButton.Play += OnPlay;
+	}
+	
+
+	// Update is called once per frame
+	void OnPlay () {
+        MoveInput.KeyAction += Flip;
+        MoveInput.CrouchAction += Crouch;
+        MoveInput.StandingAction += Standing;
+        PlayButton.Play -= OnPlay;
+	}
+
+    void Crouch()
+    {
+        rotValue.z = 90;
+
+        myRotate.eulerAngles = rotValue;
+        transform.rotation = myRotate;
+    }
+
+    void Standing()
+    {
+        rotValue.z = 0;
+
+        myRotate.eulerAngles = rotValue;
+        transform.rotation = myRotate;
+    }
+       
+    private void Flip(float obj)
+    {
+        if (obj > 0)
+            rotValue.y = 0;
+        if (obj < 0)
+            rotValue.y = 180;
+
+        myRotate.eulerAngles = rotValue;
+        transform.rotation = myRotate;
+    }
+}
