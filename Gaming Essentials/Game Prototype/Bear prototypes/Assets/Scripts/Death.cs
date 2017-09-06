4b3cdb3c5d256e8bbacb8f1c6654799c81ceb5b5
Doctoremm <doctoremm@gmail.com>
2017-09-06 14:28:27 -0600

    Hope this works

---
+++ Gaming Essentials/Game Prototype/Bear prototypes/Assets/Scripts/Death.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Death : MonoBehaviour {
+
+    public static Action End;
+
+    // Use this for initialization
+    void OnTriggerEnter()
+    {
+        End();
+    }
+}
