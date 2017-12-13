a5723f0d92fbab54c74d9ed2db719c451f6067d9
Doctoremm <doctoremm@gmail.com>
2017-12-12 20:18:48 -0700

    Things working, and then things not working.  WHY?!?!

---
+++ Gaming Essentials/Game Prototype/BearBears/Assets/Scripts/SetActive.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActive : MonoBehaviour {
+
+public GameObject Active;
+
+    private void OnTriggerEnter()
+    {
+        Active.SetActive(true);
+    }
+}
